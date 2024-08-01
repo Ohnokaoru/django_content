e3d0b9257a4dd00be67430c3564657a78ddc93f0
Kaoru <kaoru@gmail.com>
2024-08-01 17:39:56 +0800

    1.建立template/base.html 2.製作register的html與view.py 還沒debug

---
+++ app01/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+# 內建form
+from django.contrib.auth.forms import UserCreationForm
+
+# 內建models
+from django.contrib.auth.models import User
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+# 內建引入form
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
+
+
+# 內建表單會自動驗證
+def user_register(request):
+    message = ""
+
+    # 建空表單給GET方法渲染用，一般進入頁面就是GET方法
+    form = UserCreationForm()
+
+    # 按下提交表單後
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        # form是否有效，會自動驗證UserCreationForm的帳密條件，並創建
+        if form.is_valid:
+            form.save()
+
+    return render(request, "app01/user-register.html", locals())
