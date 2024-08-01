daf39e8adc60263bb5b23e70847ba61148ccf0f2
Kaoru <kaoru@gmail.com>
2024-08-01 17:55:00 +0800

    完成註冊帳號debug

--- app01/views.py
+++ app01/views.py
@@ -8,8 +8,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 # 內建表單會自動驗證
 def user_register(request):
-    message = ""
-
     # 建空表單給GET方法渲染用，一般進入頁面就是GET方法
     form = UserCreationForm()
 
@@ -18,7 +16,7 @@ def user_register(request):
         form = UserCreationForm(request.POST)
 
         # form是否有效，會自動驗證UserCreationForm的帳密條件，並創建
-        if form.is_valid:
+        if form.is_valid():
             form.save()
 
     return render(request, "app01/user-register.html", locals())
